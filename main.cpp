8c5fd22dd421889e0591390f2b5b534b0e663734
depasca <depasca@gmail.com>
2021-05-16 19:31:24 +0200

    expanded classes a bit more.. WIP

--- main.cpp
+++ main.cpp
@@ -1,35 +1,98 @@
+#include <iostream>
 #include <vector>
 #include <random>
 
 class Solution
 {
-public:
-    Solution(vector<vector<int>> sol)
+    vector<double> accountPrice;
+    vector<double> stats;
+
+    void calculateAccountPrices(const vector<double> &brokerPrice)
     {
+        for (int i = 0; i < sol.size(); i++)
+        {
+            long qty = 0;
+            accountPrice[i] = 0;
+            for (int j = 0; j < sol[i].size(); j++)
+            {
+                qty += sol[i][j];
+                accountPrice[i] += sol[i][j] * brokerPrice[j];
+            }
+            accountPrice[i] /= qty;
+        }
+    }
+    void calculateStats()
+    {
+        stats[0] = stats[1] = 0;
+        for (int i = 0; i < accountPrice.size(); i++)
+        {
+            stats[0] += accountPrice[i];
+        }
+        stats[0] /= accountPrice.size();
+        for (int i = 0; i < accountPrice.size(); i++)
+        {
+            stats[1] += (accountPrice[i] - stats[0]) * (accountPrice[i] - stats[0]);
+        }
+        stats[1] /= accountPrice.size();
+    }
+
+public:
+    Solution(vector<vector<int>> sol, const vector<double> &brokerPrice)
+    {
+        accountPrice = new vector<double>(sol.size());
+        stats = new vector<double>(2);
+        calculateAccountPrices(brokerPrice);
+        calculateStats();
     }
     void applyMutation() {}
     bool isCorrect() {}
-    //price mean and std
-    vector<double> getStats() {}
+    //price mean and MSE
+    vector<double> getStats() { return stats; }
 };
 
 class PriceAllocator
 {
     int firstGenSize;
+    int genCount;
+    vector<double> bestSolution;
     vector<Solution> population;
     double getFitnessScore(Solution s) {}
     vector<Solution> getNewGeneration() {}
 
 public:
-    PriceAllocator(vector<int> &acctQty, vector<int> &brokerQty, vector<vector<int>> &matrix)
+    PriceAllocator(int firstGenSize, int crossoverType, int maxIterations, int eliteSelectionNumber)
     {
     }
-    evolve(int maxIterations) {}
+    void setInput(vector<int> &acctQty, vector<int> &brokerQty, vector<double> &brokerPrice, vector<vector<int>> &matrix)
+    {
+    }
+
+    run()
+    {
+        // firt generation
+        // loop until target or max iterations
+        //  calc stats for each sol
+        //  select elite
+        //  new generation
+        //  mutations
+    }
 };
 
 int main(int argc, char **argv)
 {
+    //read config
+    // firstGenSize
+    // crossoverType (single, onebyone)
+    // maxIterations
+    // priceMSETarget
+    // eliteSelectionNumber
+    // mutationProbThreshold
+
     //read input from file
+    // acctQtyList
+    // brokerQtyList
+    // brokerMatrix
+
     //kick off
     //
 }
